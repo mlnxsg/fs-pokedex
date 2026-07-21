66a0f1eb4c82a9a3e2d43f64e36e35a0bab63ff3
mlnxsg <1727091819@qq.com>
2026-07-21 18:44:33 +0800

    e2e

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,15 @@
+import { test, describe, expect } from '@playwright/test'
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('can navigate to a pokemon page', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
