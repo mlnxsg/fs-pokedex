f4c05966b23247411ebd8dbc6c1c6ee3e59f9d0d
mlnxsg <1727091819@qq.com>
2026-07-22 14:27:21 +0800

    health check

--- app.js
+++ app.js
@@ -9,15 +9,13 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
 app.use(express.static('dist'))
 
 const start = async () => {
-  await app.listen(PORT+1)
+  await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
 }
 
